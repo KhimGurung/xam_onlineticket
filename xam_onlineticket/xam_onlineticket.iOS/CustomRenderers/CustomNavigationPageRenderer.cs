033a9934d1ba9a2b2efb52e579e66c6051a279cf
Khim-Bahadur Gurung <khimbahadurgurung@OBCCAdmins-MacBook-Pro.local>
2020-04-21 18:33:42 +0200

    remove navigationbar seperator in ios in custom navigation page

--- xam_onlineticket/xam_onlineticket.iOS/CustomRenderers/CustomNavigationPageRenderer.cs
+++ xam_onlineticket/xam_onlineticket.iOS/CustomRenderers/CustomNavigationPageRenderer.cs
@@ -5,19 +5,30 @@ using xam_onlineticket.CustomControls;
 using xam_onlineticket.iOS.CustomRenderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 [assembly: ExportRenderer(typeof(CustomNavigationPage), typeof(CustomNavigationPageRenderer))]
 namespace xam_onlineticket.iOS.CustomRenderers
 {
     public class CustomNavigationPageRenderer : NavigationRenderer
     {
-        public override void ViewDidLoad()
+        public override void ViewWillAppear(bool animated)
         {
-            base.ViewDidLoad();
-            // remove lower border and shadow of the navigation bar
-            NavigationBar.SetBackgroundImage(new UIImage(), UIBarMetrics.Default);
-            NavigationBar.ShadowImage = new UIImage();
+            base.ViewWillAppear(animated);
 
+            if (Element is Xamarin.Forms.NavigationPage navigationPage)
+            {
+                if (navigationPage.OnThisPlatform().HideNavigationBarSeparator())
+                {
+                    //NavigationBar.SetBackgroundImage(new UIImage(), UIBarMetrics.Default);
+                    //NavigationBar.ShadowImage = new UIImage();
+                    if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+                    {
+                        NavigationBar.StandardAppearance.ShadowColor = null;
+                        //NavigationBar.ScrollEdgeAppearance.ShadowColor = null;
+                    }
+                }
+            }
         }
     }
 }
--- xam_onlineticket/xam_onlineticket/CustomControls/CustomNavigationPage.cs
+++ xam_onlineticket/xam_onlineticket/CustomControls/CustomNavigationPage.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace xam_onlineticket.CustomControls
 {
-    public class CustomNavigationPage : NavigationPage
+    public class CustomNavigationPage : Xamarin.Forms.NavigationPage
     {
-        public CustomNavigationPage(Page page) : base(page) { }
+        public CustomNavigationPage() : base(){}
+
+        public CustomNavigationPage(Xamarin.Forms.Page page) : base(page)
+        {
+            On<iOS>().SetHideNavigationBarSeparator(true);
+        }
     }
 }
 
